8c66fc7ffd6cd2df8f4a175c7719900b735e09fa
MartijnW <weningmartijn@gmail.com>
2025-01-24 12:27:16 +0100

    Add project files.

---
+++ VICTUZ-studievereniging-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VICTUZ_studievereniging_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VICTUZ-studievereniging-app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VICTUZ_studievereniging_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
