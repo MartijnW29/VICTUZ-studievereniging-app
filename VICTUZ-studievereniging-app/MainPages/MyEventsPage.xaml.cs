33abaec71e5b0c6b80d8ab6c1d4abff871b8b2f8
MartijnW <weningmartijn@gmail.com>
2025-01-27 10:43:58 +0100

    werkende knop om myeventspage

--- VICTUZ-studievereniging-app/MainPages/MyEventsPage.xaml.cs
+++ VICTUZ-studievereniging-app/MainPages/MyEventsPage.xaml.cs
@@ -1,9 +1,28 @@
-namespace VICTUZ_studievereniging_app.MainPages;
-
-public partial class MyEventsPage : ContentPage
+namespace VICTUZ_studievereniging_app.MainPages
 {
-	public MyEventsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class MyEventsPage : ContentPage
+    {
+        public MyEventsPage()
+        {
+            InitializeComponent();
+        }
+
+        // Wanneer de gebruiker op de knop "Maak Event Aan" klikt
+        private async void OnCreateEventClicked(object sender, EventArgs e)
+        {
+            // Toon een simpele popup voor de naam en beschrijving van het evenement
+            string eventName = await DisplayPromptAsync("Evenementnaam", "Voer de naam van het evenement in");
+            string eventDescription = await DisplayPromptAsync("Beschrijving", "Voer een beschrijving in");
+
+            if (string.IsNullOrEmpty(eventName) || string.IsNullOrEmpty(eventDescription))
+            {
+                await DisplayAlert("Fout", "Vul alle velden in", "OK");
+            }
+            else
+            {
+                // Hier kun je het evenement opslaan in je database of in een lijst
+                await DisplayAlert("Succes", $"Evenement '{eventName}' is aangemaakt!", "OK");
+            }
+        }
+    }
 }
