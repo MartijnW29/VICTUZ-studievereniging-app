d8b39ab9cab075e64effc0a42fc468fa8754030f
MartijnW <weningmartijn@gmail.com>
2025-01-31 09:31:52 +0100

    profile smal fix

--- VICTUZ-studievereniging-app/MainPages/ProfilePage.xaml.cs
+++ VICTUZ-studievereniging-app/MainPages/ProfilePage.xaml.cs
@@ -10,8 +10,16 @@ namespace VICTUZ_studievereniging_app.MainPages
         public ProfilePage()
         {
             InitializeComponent();
-            BindingContext = App.CurrentUser;
             localdatabase = new SQLiteDatabase();
+
+            if (App.CurrentUser != null)
+            {
+                BindingContext = App.CurrentUser;
+            }
+            else
+            {
+                DisplayAlert("Fout", "Gebruikersgegevens niet beschikbaar.", "OK");
+            }
         }
 
         private async void OnLogoutButtonClicked(object sender, EventArgs e)
@@ -19,26 +27,22 @@ namespace VICTUZ_studievereniging_app.MainPages
             bool confirm = await DisplayAlert("Uitloggen", "Weet je zeker dat je wilt uitloggen?", "Ja", "Nee");
             try
             {
+                if (confirm)
+                {
+                    // Verwijder de huidige gebruiker uit de lokale database
+                    await localdatabase.DeleteUserAsync(App.CurrentUser.Id);
 
-            
-            if (confirm)
-            {
-                // Verwijder de huidige gebruiker uit de lokale database
-                //await localdatabase.DeleteLoggedInUserAsync();
-                await localdatabase.DeleteUserAsync(App.CurrentUser.Id);
+                    // Maak de CurrentUser in de app leeg
+                    App.CurrentUser = null;
 
-                // Maak de CurrentUser in de app leeg
-                App.CurrentUser = null;
-
-                // Navigeer terug naar de inlogpagina
-                Application.Current.MainPage = new LoginPage();
-            }
+                    // Navigeer terug naar de inlogpagina
+                    Application.Current.MainPage = new LoginPage();
+                }
             }
             catch (Exception ex)
             {
                 await DisplayAlert("Fout", $"Er is iets misgegaan: {ex.Message}", "OK");
             }
-            
         }
     }
 }
